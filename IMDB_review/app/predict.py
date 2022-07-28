9060c23145b1f0c2d8831847ce4d5ae82e6efa40
Ajeet <akv1087@gmail.com>
2022-07-28 14:43:06 +0530

    Corrected the pickle file

--- IMDB_review/app/predict.py
+++ IMDB_review/app/predict.py
@@ -19,14 +19,14 @@ class Predict:
         print('writing tf_idf pickle file')
         tfidf = TfidfVectorizer(strip_accents = None, lowercase = False, preprocessor = None,  use_idf = True, norm = 'l2', smooth_idf = True)
         X = tfidf.fit_transform(text)
-        pickle.dump(X , open(r"IMDB_review\app\pickle\tf_idf.pickle" , "wb"))
         print(X.shape)
+        pickle.dump(tfidf, open(r"app\pickle\tf_idf.pickle" , "wb"))
         return X
 
 
     def loadTransform_tf_idf(self, text):
         print('load tf_idf function')
-        tf_idf = pickle.load(open(r"IMDB_review\app\pickle\tf_idf.pickle" , "rb"))
+        tf_idf = pickle.load(open(r"app\pickle\tf_idf.pickle" , "rb"))
         X = tf_idf.transform(text)
         return X
 
@@ -34,11 +34,11 @@ class Predict:
         xgb = XGBClassifier()
         xgb.fit(X,y)
         print('train successful')
-        pickle.dump(xgb, open(r"IMDB_review\app\pickle\xgb.pickle", "wb"))
+        pickle.dump(xgb, open(r"app\pickle\xgb.pickle", "wb"))
         
 
     def load_predict(self, X):
-        xgb = pickle.load(open(r"IMDB_review\app\pickle\xgb.pickle", "rb"))
+        xgb = pickle.load(open(r"app\pickle\xgb.pickle", "rb"))
         y = xgb.predict(X)
         return y
 
